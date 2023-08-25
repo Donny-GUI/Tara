02f7313cfccf3f8f331688f96ede567e4887fc1a
Donny <108424001+Donny-GUI@users.noreply.github.com>
2023-08-24 19:51:44 -0700

    new file:   util.py 	deleted:    README.md 	modified:   add_type_hints.py 	deleted:    boolean_variables.pkl 	deleted:    bytes_variables.pkl 	modified:   collect_types.py 	deleted:    dict_variables.pkl 	deleted:    float_variables.pkl 	deleted:    int_variables.pkl 	deleted:    list_variables.pkl 	deleted:    string_variables.pkl 	data/

---
+++ util.py
@@ -0,0 +1,34 @@
+import os
+import pickle
+
+
+def import_lists():
+    with open(os.getcwd()+os.sep+"data"+os.sep+"list_variables.pkl", "r") as f:
+        return pickle.load(f)
+
+def import_booleans():
+    with open(os.getcwd()+os.sep+"data"+os.sep+'boolean_variables.pkl', 'rb') as file:
+        return pickle.load(file)
+
+def import_bytes():
+    with open(os.getcwd()+os.sep+"data"+os.sep+'bytes_variables.pkl', 'rb') as file:
+        return pickle.load(file)
+
+def import_dicts():
+    with open(os.getcwd()+os.sep+"data"+os.sep+'dict_variables.pkl', 'rb') as file:
+        return pickle.load(file)
+
+def import_ints():
+    with open(os.getcwd()+os.sep+"data"+os.sep+'int_variables.pkl', 'rb') as file:
+        return pickle.load(file)
+
+def import_strings():
+    with open(os.getcwd()+os.sep+"data"+os.sep+'string_variables.pkl', 'rb') as file:
+        return pickle.load(file)
+
+BoolMap = import_booleans()
+ListMap = import_lists()
+BytesMap = import_bytes()
+DictMap = import_dicts()
+StringMap = import_strings()
+IntMap = import_ints()
