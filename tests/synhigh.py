0174bf731ae7316586ef53d21682fca268b6a4b4
Donny <108424001+Donny-GUI@users.noreply.github.com>
2023-09-02 10:59:35 -0700

    deleted:    __pycache__/add_type_hints.cpython-311.pyc 	deleted:    __pycache__/tests.cpython-311.pyc 	deleted:    __pycache__/util.cpython-311.pyc 	deleted:    tara/__pycache__/__init__.cpython-311.pyc 	deleted:    tara/__pycache__/app.cpython-311.pyc 	deleted:    tara/__pycache__/doctor.cpython-311.pyc 	deleted:    tara/__pycache__/train_types.cpython-311.pyc 	deleted:    tara/__pycache__/type_hints.cpython-311.pyc 	deleted:    tara/__pycache__/util.cpython-311.pyc 	deleted:    tests.py 	new file:   tests/synhigh.py 	deleted:    typed_level.py 	deleted:    typed_player.py 	deleted:    typed_settings.py

---
+++ tests/synhigh.py
@@ -0,0 +1,278 @@
+import tokenize
+import tkinter as tk
+from tkinter import Menu, messagebox, filedialog, simpledialog
+import keyword
+import re
+from tkinter import ttk
+import builtins
+from typing import Optional, Tuple, Union
+import customtkinter as ctk
+from pyautogui import size
+from io import BytesIO
+
+
+DimGrayRGB = (51, 51, 51)
+DimGrayHTML = "#333333"
+DimGrayName = "dimgray"
+
+html_colors = {
+    "aliceblue": "#F0F8FF",
+    "antiquewhite": "#FAEBD7",
+    "aqua": "#00FFFF",
+    "aquamarine": "#7FFFD4",
+    "azure": "#F0FFFF",
+    "beige": "#F5F5DC",
+    "bisque": "#FFE4C4",
+    "black": "#000000",
+    "blanchedalmond": "#FFEBCD",
+    "blue": "#0000FF",
+    "blueviolet": "#8A2BE2",
+    "brown": "#A52A2A",
+    "burlywood": "#DEB887",
+    "cadetblue": "#5F9EA0",
+    "chartreuse": "#7FFF00",
+    "chocolate": "#D2691E",
+    "coral": "#FF7F50",
+    "cornflowerblue": "#6495ED",
+    "cornsilk": "#FFF8DC",
+    "crimson": "#DC143C",
+    "cyan": "#00FFFF",
+    "darkblue": "#00008B",
+    "darkcyan": "#008B8B",
+    "darkgoldenrod": "#B8860B",
+    "darkgray": "#A9A9A9",
+    "darkgreen": "#006400",
+    "darkkhaki": "#BDB76B",
+    "darkmagenta": "#8B008B",
+    "darkolivegreen": "#556B2F",
+    "darkorange": "#FF8C00",
+    "darkorchid": "#9932CC",
+    "darkred": "#8B0000",
+    "darksalmon": "#E9967A",
+    "darkseagreen": "#8FBC8F",
+    "darkslateblue": "#483D8B",
+    "darkslategray": "#2F4F4F",
+    "darkturquoise": "#00CED1",
+    "darkviolet": "#9400D3",
+    "deeppink": "#FF1493",
+    "deepskyblue": "#00BFFF",
+    "dimgray": "#696969",
+    "dodgerblue": "#1E90FF",
+    "firebrick": "#B22222",
+    "floralwhite": "#FFFAF0",
+    "forestgreen": "#228B22",
+    "fuchsia": "#FF00FF",
+    "gainsboro": "#DCDCDC",
+    "ghostwhite": "#F8F8FF",
+    "gold": "#FFD700",
+    "goldenrod": "#DAA520",
+    "gray": "#808080",
+    "green": "#008000",
+    "greenyellow": "#ADFF2F",
+    "honeydew": "#F0FFF0",
+    "hotpink": "#FF69B4",
+    "indianred": "#CD5C5C",
+    "indigo": "#4B0082",
+    "ivory": "#FFFFF0",
+    "khaki": "#F0E68C",
+    "lavender": "#E6E6FA",
+    "lavenderblush": "#FFF0F5",
+    "lawngreen": "#7CFC00",
+    "lemonchiffon": "#FFFACD",
+    "lightblue": "#ADD8E6",
+    "lightcoral": "#F08080",
+    "lightcyan": "#E0FFFF",
+    "lightgoldenrodyellow": "#FAFAD2",
+    "lightgray": "#D3D3D3",
+    "lightgreen": "#90EE90",
+    "lightpink": "#FFB6C1",
+    "lightsalmon": "#FFA07A",
+    "lightseagreen": "#20B2AA",
+    "lightskyblue": "#87CEFA",
+    "lightslategray": "#778899",
+    "lightsteelblue": "#B0C4DE",
+    "lightyellow": "#FFFFE0",
+    "lime": "#00FF00",
+    "limegreen": "#32CD32",
+    "linen": "#FAF0E6",
+    "magenta": "#FF00FF",
+    "maroon": "#800000",
+    "mediumaquamarine": "#66CDAA",
+    "mediumblue": "#0000CD",
+    "mediumorchid": "#BA55D3",
+    "mediumpurple": "#9370DB",
+    "mediumseagreen": "#3CB371",
+    "mediumslateblue": "#7B68EE",
+    "mediumspringgreen": "#00FA9A",
+    "mediumturquoise": "#48D1CC",
+    "mediumvioletred": "#C71585",
+    "midnightblue": "#191970",
+    "mintcream": "#F5FFFA",
+    "mistyrose": "#FFE4E1",
+    "moccasin": "#FFE4B5",
+    "navajowhite": "#FFDEAD",
+    "navy": "#000080",
+    "oldlace": "#FDF5E6",
+    "olive": "#808000",
+    "olivedrab": "#6B8E23",
+    "orange": "#FFA500",
+    "orangered": "#FF4500",
+    "orchid": "#DA70D6",
+    "palegoldenrod": "#EEE8AA",
+    "palegreen": "#98FB98",
+    "paleturquoise": "#AFEEEE",
+    "palevioletred": "#DB7093",
+    "papayawhip": "#FFEFD5",
+    "peachpuff": "#FFDAB9",
+    "peru": "#CD853F",
+    "pink": "#FFC0CB",
+    "plum": "#DDA0DD",
+    "powderblue": "#B0E0E6",
+    "purple": "#800080",
+    "rebeccapurple": "#663399",
+    "red": "#FF0000",
+    "rosybrown": "#BC8F8F",
+    "royalblue": "#4169E1",
+    "saddlebrown": "#8B4513",
+    "salmon": "#FA8072",
+    "sandybrown": "#F4A460",
+    "seagreen": "#2E8B57",
+    "seashell": "#FFF5EE",
+    "sienna": "#A0522D",
+    "silver": "#C0C0C0",
+    "skyblue": "#87CEEB",
+    "slateblue": "#6A5ACD",
+    "slategray": "#708090",
+    "snow": "#FFFAFA",
+    "springgreen": "#00FF7F",
+    "steelblue": "#4682B4",
+    "tan": "#D2B48C",
+    "teal": "#008080",
+    "thistle": "#D8BFD8",
+    "tomato": "#FF6347",
+    "turquoise": "#40E0D0",
+    "violet": "#EE82EE",
+    "wheat": "#F5DEB3",
+    "white": "#FFFFFF",
+    "whitesmoke": "#F5F5F5",
+    "yellow": "#FFFF00",
+    "yellowgreen": "#9ACD32",
+}
+
+
+def python_builtin_regex():        
+    python_builtins = dir(builtins)
+    builtins_patterns = [rf'\b{re.escape(builtin)}\b' for builtin in python_builtins]
+    builtins_regex = '|'.join(builtins_patterns)
+    return re.compile(builtins_regex)
+
+def python_keyword_regex():
+    python_keywords = keyword.kwlist
+    keyword_patterns = [rf'\b{re.escape(keyword)}\b' for keyword in python_keywords]
+    keywords_regex = '|'.join(keyword_patterns)
+    return re.compile(keywords_regex)
+
+
+class CodeEditorApp:
+    def __init__(self, root):
+        self.label_style = ttk.Style()
+        self.label_style.configure("Dark.Label", foreground="white", background=DimGrayHTML)
+
+        self.size = size()
+        self.half_width = self.size.width // 2
+
+        self.root:tk.Tk = root
+        self.root.configure(bg="#333333")
+        self.root.title("Code Editor")
+        self.root.geometry(f"{self.half_width}x{self.size.height}")
+
+
+        # Create a Text widget for code editing
+        self.text_widget = tk.Text(root, wrap=tk.WORD, background=DimGrayHTML, foreground="white", font="consolas 12")
+        self.text_widget.pack(fill=tk.BOTH, expand=True)
+
+        self.scrollbar = ttk.Scrollbar(root, command=self.text_widget.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.text_widget.config(yscrollcommand=self.scrollbar.set)
+
+        # Bind the event to trigger syntax highlighting
+        self.text_widget.bind("<KeyRelease>", self.syntaxhighlight)
+
+
+        # Create a Menu bar
+        self.menu_bar = Menu(self.root)
+        self.root.config(menu=self.menu_bar)
+
+        # Create File menu
+        self.file_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="New", command=self.new_file)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.exit_app)
+
+        # Create Edit menu
+        self.edit_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+        self.edit_menu.add_command(label="Cut", command=self.cut_text)
+        self.edit_menu.add_command(label="Copy", command=self.copy_text)
+        self.edit_menu.add_command(label="Paste", command=self.paste_text)
+
+    def syntaxhighlight(self, *args):
+        self.code = self.text_widget.get("1.0", tk.END)
+
+        def makecode(code:str):
+            try:
+                stream = BytesIO(code.encode('utf-8'))
+                tokens = list(tokenize.tokenize(stream.readline))
+            except tokenize.TokenError:
+                code = code[:-1]
+                makecode(code)
+            return stream, tokens
+        
+        self.stream, self.tokens = makecode(self.code)
+        for token in self.tokens:
+            print(token.string, end="")
+            
+
+
+
+    def new_file(self):
+        self.text_widget.delete("1.0", tk.END)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+        if file_path:
+            with open(file_path, "r") as file:
+                file_content = file.read()
+                self.text_widget.delete("1.0", tk.END)
+                self.text_widget.insert("1.0", file_content)
+
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+        if file_path:
+            with open(file_path, "w") as file:
+                file_content = self.text_widget.get("1.0", tk.END)
+                file.write(file_content)
+
+    def exit_app(self):
+        self.root.quit()
+
+    def cut_text(self):
+        self.text_widget.event_generate("<<Cut>>")
+
+    def copy_text(self):
+        self.text_widget.event_generate("<<Copy>>")
+
+    def paste_text(self):
+        self.text_widget.event_generate("<<Paste>>")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CodeEditorApp(root)
+    root.mainloop()
+
+
+    
+
